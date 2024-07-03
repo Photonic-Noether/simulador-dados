b7c20bc48fc569e7f50cada0bb91e5341d3feb6d
Joaquin Hernandez Martinez <proyecto_noether@outlook.com>
2024-07-03 14:08:37 +0200

    Añadida interfaz y ahora el dado es de N caras

--- dado.py
+++ dado.py
@@ -1,4 +1,18 @@
 import random
 
-numero_aleatorio = random.randint(1,6)
-print(numero_aleatorio)
+INTERFAZ = """
+
+#####################################
+### SCRIPT DE SIMULACION DE DADOS ###
+#####################################
+
+Este script simula la tirada de un dado
+
+"""
+
+print(INTERFAZ)
+
+numero_caras = int(input("De cuantas caras quieres el dado: "))
+dado = random.randint(1,numero_caras)
+
+print(f"El resultado de tu tirada es {dado}")
