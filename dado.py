628ee260b7fc67e0eff9e6b2f9f9334ad2f0c78d
Joaquin Hernandez Martinez <proyecto_noether@outlook.com>
2024-07-03 14:25:39 +0200

    Separado en modulos para tener punto de entrada (main.py), dados (dado.py) e interfaz (interfaz.py)

--- dado.py
+++ dado.py
@@ -1,26 +1,7 @@
 import random
-
-INTERFAZ = """
-
-#####################################
-### SCRIPT DE SIMULACION DE DADOS ###
-#####################################
-
-Este script simula la tirada de un dado
-
-"""
-
-print(INTERFAZ)
-
 class Dado:
     def __init__(self, numero_caras: int):
         self.numero_caras = numero_caras
         
     def tirada(self) -> int:
         return random.randint(1, self.numero_caras)
-
-numero_caras = int(input("De cuantas caras quieres el dado: "))
-dado = Dado(numero_caras)
-tirada = dado.tirada()
-
-print(f"El resultado de tu tirada es {tirada}")
---
+++ interfaz.py
@@ -0,0 +1,9 @@
+INTERFAZ = """
+
+#####################################
+### SCRIPT DE SIMULACION DE DADOS ###
+#####################################
+
+Este script simula la tirada de un dado
+
+"""
